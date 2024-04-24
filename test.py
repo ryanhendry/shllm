1e322114b6d024d5bfece5edc9b05882f0c2728e
Ryan Hendry <ryanhendry123@gmail.com>
2024-04-24 21:41:05 +0700

    .

--- test.py
+++ test.py
@@ -2,4 +2,5 @@ import subprocess
 
 def test_shll_ping_localhost():
     output = subprocess.check_output(["./shll", "command to ping my localhost"]).decode("utf-8").strip()
+    print(output)
     assert "Command to be copied to clipboard: ping localhost" in output
